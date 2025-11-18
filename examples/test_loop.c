20fb32ba9a768c0b3c0cc00a8746c483e617bac0
Michael El Omar <michaelelomar@outlook.com>
2025-11-18 18:00:42 +0100

    implement an event loop based on epoll system call

--- examples/test_loop.c
+++ examples/test_loop.c
@@ -1,7 +1,47 @@
 #include <syskit.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+
+int running = 1;
+
+void sig_handler(int signum)
+{
+	LOGI("Received signal=%s", strsignal(signum));
+	running = 0;
+}
+
+void callback(int fd, uint32_t revents, void *userdata)
+{
+	LOGD("Inside %s, events: %d", __func__, revents);
+}
 
 int main(int argc, char *argv[])
 {
-	test_function();
+	/* exit on SIGINT & SIGTERM */
+	signal(SIGINT, &sig_handler);
+	signal(SIGTERM, &sig_handler);
+
+	/* ignore SIGPIPE */
+	signal(SIGPIPE, SIG_IGN);
+
+	struct syskit_loop *loop = syskit_loop_create();
+
+	int fd = open("/dev/input/mouse0", O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		return -errno;
+	}
+
+	syskit_loop_add(loop, fd, callback, SYSKIT_FD_EVENT_IN);
+
+	while (running) {
+		syskit_loop_spin(loop);
+		LOGI("Here");
+	}
+
+	close(fd);
+	syskit_loop_destroy(loop);
+
 	return 0;
 }
--- include/syskit.h
+++ include/syskit.h
@@ -1,1 +1,37 @@
-#include <loop.h>
+#include <priv.h>
+
+struct syskit_loop;
+struct syskit_evt;
+
+enum syskit_fd_event {
+	SYSKIT_FD_EVENT_IN = 0x001,
+	SYSKIT_FD_EVENT_PRI = 0x002,
+	SYSKIT_FD_EVENT_OUT = 0x004,
+	SYSKIT_FD_EVENT_ERROR = 0x008,
+	SYSKIT_FD_EVENT_HUP = 0x010,
+};
+
+typedef void (*syskit_fd_event_cb)(int fd, uint32_t revents, void *userdata);
+
+/* loop public API */
+
+struct syskit_loop *syskit_loop_create();
+
+int syskit_loop_add(struct syskit_loop *loop,
+		    int fd,
+		    syskit_fd_event_cb cb,
+		    uint32_t events);
+
+int syskit_loop_remove(struct syskit_loop *loop, int fd);
+
+int syskit_loop_spin(struct syskit_loop *loop);
+
+void syskit_loop_display_registered_fds(struct syskit_loop *loop);
+
+void syskit_loop_destroy(struct syskit_loop *loop);
+
+/* event public API */
+
+struct syskit_evt *syskit_evt_create();
+
+void syskit_evt_destroy(struct syskit_evt *evt);
